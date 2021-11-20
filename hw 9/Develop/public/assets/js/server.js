c409ba11859777cd446e8f1951ebb54a3631c2d4
Kai <mordechaigerstley@gmail.com>
2021-11-20 11:33:31 -0500

    deployed it to heroku

--- hw 9/Develop/public/assets/js/server.js
+++ hw 9/Develop/public/assets/js/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const apiRoutes = require("./routes/apiroutes");
+const apiRoutes = require("./routes/data");
 
 const PORT = process.env.PORT || 3001;
 
--- hw 9/Develop/routes/data.js
+++ hw 9/Develop/routes/data.js
@@ -4,15 +4,12 @@ const fs = require("fs");
 const notes = require("../db/db.json");
 const { v4: uuidv4 } = require("uuid");
 
-// GET Route for retrieving notes
 router.get("/api/notes", (req, res) => {
   console.info(`${req.method} request received for tips`);
   res.status(200).json(notes);
 });
 
-// POST Route for adding notes
 router.post("/api/notes", (req, res) => {
-  // Log that a POST request was received
   console.info(`${req.method} request received to add a tip`);
 
   const { title, text, id } = req.body;
@@ -26,7 +23,6 @@ router.post("/api/notes", (req, res) => {
     notes.push(newNote);
     const final = JSON.stringify(notes);
 
-    // Write the string to a file
     fs.writeFile(`./db/db.json`, final, (err) =>
       err
         ? console.error(err)
